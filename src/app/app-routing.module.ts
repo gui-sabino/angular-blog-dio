7fd65b426c22076d078716e4d7232c7d85f58c97
guisabinou <gui.sabinof@gmail.com>
2023-05-30 20:25:18 -0300

    estilização mais interessante

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,9 @@ import { ContentComponent } from './pages/content/content.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  {
-    path:'',
-    component:HomeComponent
-  },
-  {
-    path:'content',
-    component:ContentComponent
-  }
+  {path: '', component: HomeComponent, pathMatch:'full'},
+  {path: 'content', component: ContentComponent },
+  {path: '**', redirectTo:''} // ROTA CORINGA
 ];
 
 @NgModule({
